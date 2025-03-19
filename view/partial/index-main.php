426403b1dfe7da8b4a5c1d70688cabfc6e6e328b
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-19 17:31:56 +0100

    Main Olympe

--- view/partial/index-main.php
+++ view/partial/index-main.php
@@ -4,3 +4,10 @@
     <?php require_once('perso.php'); ?>
 
 </section>
+
+<section>
+
+    <h2>Les Dieux de l'Olympe</h2>
+    <?php require_once('olympe.php'); ?>
+
+</section>
---
+++ view/partial/olympe.php
@@ -0,0 +1,6 @@
+<section>
+    <div>
+        <div><img src="../img/Athena.png" alt="Athena"></div>
+        <h3>Athena</h3>
+    </div>
+</section>
