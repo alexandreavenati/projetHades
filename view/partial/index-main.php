5bf4fce61e4c4c5ce6c46a42474232fb2863a4e6
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-20 11:11:26 +0100

    Projet hades v1

--- view/partial/index-main.php
+++ view/partial/index-main.php
@@ -1,11 +1,11 @@
-<section>
+<section id="perso">
 
     <h2>Les Personnages</h2>
     <?php require_once('perso.php'); ?>
 
 </section>
 
-<section>
+<section id="olympe">
 
     <h2>Les Dieux de l'Olympe</h2>
     <?php require_once('olympe.php'); ?>
