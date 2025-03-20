23112510a922fc068cb637d76beba5a8894a01fa
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-20 12:15:47 +0100

    Projet Hades v1.14

--- view/partial/olympe.php
+++ view/partial/olympe.php
@@ -1,21 +1,26 @@
 <section>
     <div>
+        <p>Athéna est la déesse de la sagesse, de la stratégie et des arts. Fille de Zeus, elle incarne la prudence et la guerre juste, guidant les héros par l’intelligence plutôt que par la force. Elle est aussi la protectrice des artisans et des philosophes.</p>
         <h3>Athena</h3>
     </div>
 
     <div>
+        <p>Zeus est le Roi des dieux de l'Olympe. Maître du ciel et du tonnerre, il incarne l'autorité, la justice et l'ordre. Contrairement à Poséidon, qui règne sur les mers, Zeus détient le pouvoir suprême sur les cieux et les éléments.</p>
         <h3>Zeus</h3>
     </div>
 
     <div>
+        <p>Artémis est la Déesse de la chasse, de la nature et de la lune. Contrairement à Aphrodite, elle incarne l'indépendance et la force. Protectrice des jeunes filles et des animaux, elle veille sur la nature sauvage avec sa maîtrise de l'archerie.</p>
         <h3>Artemis</h3>
     </div>
 
     <div>
+        <p>Déméter est la Déesse de l'agriculture et des récoltes de l'Olympe. Contrairement à Artémis, qui est la déesse de la chasse, Déméter incarne la fertilité de la terre et le cycle de la vie végétale. Elle protège les moissons et veille à l'abondance des cultures, mais sa colère peut plonger le monde dans la famine si elle est contrariée.</p>
         <h3>Demeter</h3>
     </div>
 
     <div>
+        <p>Poséidon est le Dieu des mers et des océans de l'Olympe. Contrairement à Zeus, maître du ciel, Poséidon incarne la puissance brute et impétueuse des eaux, capable de calmer ou de déchaîner des tempêtes. Maître des océans, des tremblements de terre et des créatures marines, il représente la force primordiale et incontrôlable de la nature.</p>
         <h3>Poseidon</h3>
     </div>
 
