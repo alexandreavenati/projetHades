a35e525c7dc578185e2866443dbea5d45b0bbad5
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-03-20 10:43:07 +0100

    Hades Olympe

--- view/partial/olympe.php
+++ view/partial/olympe.php
@@ -1,6 +1,26 @@
 <section>
     <div>
-        <div><img src="../img/Athena.png" alt="Athena"></div>
         <h3>Athena</h3>
+    </div>
+
+    <div>
+        <h3>Zeus</h3>
+    </div>
+
+    <div>
+        <h3>Artemis</h3>
+    </div>
+
+    <div>
+        <h3>Demeter</h3>
+    </div>
+
+    <div>
+        <h3>Poseidon</h3>
+    </div>
+
+    <div>
+        <p>Arès est le Dieu de la guerre de l'Olympe. Contrairement à Athena, plus stratègique, le domaine d'Arès est celui des aspects physiques, violents et indomptables de la guerre.</p>
+        <h3>Ares</h3>
     </div>
 </section>
